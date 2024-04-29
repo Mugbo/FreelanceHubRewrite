3d5d2c520e4839a2741a35ddc2a0864215bee193
Michael Agbo <Michael.Agbo@genesys.com>
2024-04-29 07:54:28 +0100

    changed the next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "https://freelance-hub--pi.vercel.app/"], 
+    domains: ["localhost", "freelance-hub--pi.vercel.app/"], 
   },
 };
 
